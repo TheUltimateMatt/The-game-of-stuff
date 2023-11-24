5f78b58d4c75a0bc1b8978ad59a2eda97035b3ec
unknown <gabathuler.matteo@gmail.com>
2023-11-24 16:36:49 +0100

    deuxieme commit

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import pygame
 pygame.init()
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.velocity = 2
+        self.health = 100
+        self.max_health = 100
+        self.image = pygame.image.load("assets/player.png")
+        self.rect = self.image.get_rect()
+        self.rect.x = 400
+        self.rect.y = 500
+
+
 background = pygame.image.load("assets/bg.jpg")
 
 
 pygame.display.set_caption("The game of stuff")
-pygame.display.set_mode((1064, 640))
+screen = pygame.display.set_mode((1024, 720))
 
 running = True
 
 while running:
+    screen.blit(background, (0, -200))
+    pygame.display.flip()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
-
-
-
-
